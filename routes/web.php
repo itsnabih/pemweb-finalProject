910257e3e99d8661d72ef430d29fb082a55d18dc
pisjad <hafizhassajjad17@gmail.com>
2024-06-03 03:08:52 +0700

    setup index

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProdukController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', [ProdukController::class, 'index'])->name('produk.index');
 
 Route::resource('produk', ProdukController::class);
 Route::get('/produk/search', [ProdukController::class, 'search'])->name('produk.search');
+Route::get('/statistik', function () {
+    return view('produk/statistik');
+});
