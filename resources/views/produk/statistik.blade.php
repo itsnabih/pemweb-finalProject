747d4d30dbe5cb8d27ac70dad603042086e29dd6
pisjad <hafizhassajjad17@gmail.com>
2024-06-03 05:20:54 +0700

    adding font poppins

--- resources/views/produk/statistik.blade.php
+++ resources/views/produk/statistik.blade.php
@@ -4,6 +4,11 @@
     rel="stylesheet"
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
     crossorigin="anonymous"
+/><link rel="preconnect" href="https://fonts.googleapis.com" />
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+<link
+    href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+    rel="stylesheet"
 />
 <script
     src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
@@ -81,7 +86,7 @@
         display: flex;
         justify-content: center;
         align-items: center;
-        height: 100%; /* Increase the height for a larger pie chart */
+        height: 100; /* Increase the height for a larger pie chart */
     }
 
     .pie-chart-container canvas {
